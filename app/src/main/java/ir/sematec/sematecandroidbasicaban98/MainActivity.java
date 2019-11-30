3158175abdefb74aa21c23a216f2afd88df25b23
Pouya <pouyaheydary@gmail.com>
2019-11-30 10:56:49 +0330

    end of session 3

--- app/src/main/java/ir/sematec/sematecandroidbasicaban98/MainActivity.java
+++ app/src/main/java/ir/sematec/sematecandroidbasicaban98/MainActivity.java
@@ -1,9 +1,14 @@
 package ir.sematec.sematecandroidbasicaban98;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtTitle = findViewById(R.id.txtTitle);
+        txtTitle.setText("Hi");
+        EditText edtPhone = findViewById(R.id.edtPhone);
+        ImageView imgProfile = findViewById(R.id.imgProfile);
+        Button btnSave = findViewById(R.id.btnSave);
+
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Something!", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
-
-
-
 
 }
---
+++ app/src/main/java/ir/sematec/sematecandroidbasicaban98/SecondActivity.java
@@ -0,0 +1,28 @@
+package ir.sematec.sematecandroidbasicaban98;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button btnNewPage = findViewById(R.id.btnNewPage);
+
+        btnNewPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(SecondActivity.this,ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
